de5595f12eaaaf736d261640aeb965847573f589
tony72377 <tony72377@gmail.com>
2017-01-11 11:13:11 -0500

    tonys first push

---
+++ template-homepage.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: full-page-template */
+?>
+
+<?php get_header(); ?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col s6">
+        <h2 class="center-align">Left section</h2>
+      </div>
+      <div class="col s6">
+        <h2 class="center-align">Right section</h2>
+      </div>
+      <div class="col s6">
+        <h2 class="center-align">Left section</h2>
+      </div>
+      <div class="col s6">
+        <h2 class="center-align">Right section</h2>
+      </div>
+      <div class="col s6">
+        <h2 class="center-align">Left section</h2>
+      </div>
+      <div class="col s6">
+        <h2 class="center-align">Right section</h2>
+      </div>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
